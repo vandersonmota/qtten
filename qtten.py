07ff9fcd8bb8d85da92ffaf9f5a25201feeb5167
Vanderson Mota <vanderson.mota@gmail.com>
2018-01-09 22:06:55 -0200

    Initial version

---
+++ qtten.py
@@ -0,0 +1,82 @@
+import os
+import zlib
+from contextlib import contextmanager
+
+COMPRESSION = 9
+MSG_END_TOKEN = b'EOM'
+NUL_BYTE = b'\x00'
+READ_CHUNK = 2048
+INDEX_CHUNK = 50
+MSG_END_TOKEN_SIZE = 3
+
+
+class Queue:
+    def __init__(self, filename):
+        try:
+            self.q = open(filename, 'r+b')
+        except FileNotFoundError:
+            self.q = open(filename, 'w+b')
+
+    def __del__(self):
+        self._commit()
+        self.q.close()
+
+    def enqueue(self, message):
+        self.q.seek(0)
+        idx = self.q.read(INDEX_CHUNK + MSG_END_TOKEN_SIZE)
+        # emtpy file, writing dummy index
+        with self._commit():
+            if not idx:
+                self.q.write(NUL_BYTE * INDEX_CHUNK)
+                self.q.write(MSG_END_TOKEN)
+                pos = self.q.tell()
+                self.q.seek(0)
+                self.q.write(bytes(str(pos), encoding='utf-8'))
+            self.q.seek(0, 2)
+            self.q.write(zlib.compress(bytes(message, encoding='utf-8'), COMPRESSION))
+            self.q.write(MSG_END_TOKEN)
+
+    def dequeue(self):
+        self.q.seek(0)
+        first_msg = None
+        second_msg = None
+        data = b''
+        first_msg_stops_at = -1
+        second_msg_stops_at = -1
+        idx = self.q.read(INDEX_CHUNK + MSG_END_TOKEN_SIZE)
+        if idx:
+            idx = int(idx.rstrip(NUL_BYTE + MSG_END_TOKEN))
+            self.q.seek(idx)
+        else:
+            return None
+
+        while True:
+            chunk = self.q.read(READ_CHUNK)
+            if chunk == b'' or second_msg_stops_at != -1:
+                break
+            data += chunk
+            first_msg_stops_at = data.find(MSG_END_TOKEN)
+            if first_msg_stops_at != -1:
+                second_msg_stops_at = data.find(MSG_END_TOKEN, first_msg_stops_at + MSG_END_TOKEN_SIZE)
+
+        if data:
+            if first_msg_stops_at != -1:
+                with self._commit():
+                    first_msg = data[0:first_msg_stops_at]
+                    if second_msg_stops_at != -1:
+                        #updates index pointing to next message
+                        self.q.seek(0)
+                        self.q.write(bytes(str(idx + len(first_msg) + MSG_END_TOKEN_SIZE), encoding='utf-8'))
+                    else:
+                        # all msgs consumed
+                        self.q.seek(0)
+                        self.q.truncate()
+
+                return zlib.decompress(first_msg).decode('utf-8')
+        return None
+
+    @contextmanager
+    def _commit(self):
+        yield
+        self.q.flush()
+        os.fsync(self.q.fileno())
---
+++ tests.py
@@ -0,0 +1,54 @@
+import tempfile
+import sys
+import unittest
+from unittest.mock import patch
+from hypothesis.strategies import text
+from hypothesis import given
+from qtten import Queue
+
+
+class TestQtten(unittest.TestCase):
+    def setUp(self):
+        self.q = Queue(tempfile.mkstemp()[1])
+
+    @given(x=text(), y=text(), z=text())
+    def test_enqueue_dequeue(self, x, y, z):
+        self.q.enqueue(x)
+        self.q.enqueue(y)
+        self.q.enqueue(z)
+
+        self.assertEqual(x,  self.q.dequeue())
+        self.assertEqual(y,  self.q.dequeue())
+        self.assertEqual(z,  self.q.dequeue())
+
+    @given(x=text(), y=text(), z=text())
+    def test_mixed_enqueue_dequeue(self, x, y, z):
+        self.q.enqueue(x)
+        self.q.enqueue(y)
+
+        self.assertEqual(x,  self.q.dequeue())
+
+        self.q.enqueue(z)
+        self.assertEqual(y,  self.q.dequeue())
+        self.assertEqual(z,  self.q.dequeue())
+
+    def test_dequeue_empty_queue(self):
+        self.assertIsNone(self.q.dequeue())
+
+    @patch('qtten.zlib.compress')
+    def test_message_compression(self, zlib_compress):
+        zlib_compress.return_value = b'\x21\234' #bunch of bytes
+        self.q.enqueue('hey there')
+        zlib_compress.assert_called_with(b'hey there', 9)
+
+    def test_compressed_file(self):
+        big_msg = 'big msg' * 5120000
+        self.q.enqueue(big_msg)
+
+        with open(self.q.q.name, 'rb') as fd:
+            contents = fd.read()
+            self.assertLess(sys.getsizeof(contents), sys.getsizeof(big_msg))
+
+
+if __name__ == '__main__':
+    unittest.main()
