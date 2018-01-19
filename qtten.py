e75cc1b97f3c0b30d3cc0bebe06dee19563addf7
Vanderson Mota <vanderson.mota@gmail.com>
2018-01-18 22:01:41 -0200

    it is something

--- qtten.py
+++ qtten.py
@@ -1,39 +1,76 @@
 import os
 import zlib
+import mmap
 
 COMPRESSION = 9
 MSG_END_CHAR = b'\n'
 NUL_BYTE = b'\x00'
+READ_CHUNK = 2048
+INDEX_CHUNK = 50
+MSG_END_CHAR_SIZE = 1
 
 
 class Queue:
     def __init__(self, filename):
         self.filename = filename
+        try:
+            f = open(self.filename, 'r+b')
+        except FileNotFoundError:
+            f = open(self.filename, 'w')
+        finally:
+            f.close()
+
+
 
     def enqueue(self, message):
-        with open(self.filename, 'ab') as q:
+        with open(self.filename, 'r+b') as q:
+            idx = q.read(INDEX_CHUNK + MSG_END_CHAR_SIZE)
+            # emtpy file, writing dummy index
+            if not idx:
+                q.write(NUL_BYTE * INDEX_CHUNK)
+                q.write(MSG_END_CHAR)
+
+            pos = q.tell()
             q.write(zlib.compress(bytes(message, encoding='utf-8'), COMPRESSION))
             q.write(MSG_END_CHAR)
+
+            q.seek(0)
+            q.write(bytes(str(pos), encoding='utf-8'))
 
     def dequeue(self):
         with open(self.filename, 'r+b') as q:
             first_msg = None
             second_msg = None
-            first_msg = q.readline().strip()
-            if first_msg:
-                second_msg = q.readline().strip()
+            data = b''
+            second_msg_stops_at = -1
+            idx = q.read(INDEX_CHUNK + MSG_END_CHAR_SIZE)
+            if idx:
+                idx = int(idx.rstrip(NUL_BYTE + MSG_END_CHAR))
+                q.seek(idx)
             else:
                 return None
 
-            if second_msg:
-                q.seek(0)
-                new_message_size = len(first_msg) + len(second_msg) + 1
-                q.write(NUL_BYTE * new_message_size)
-                q.write(MSG_END_CHAR)
-                q.seek(0)
-                q.write(second_msg)
-            else:
-                q.seek(0)
-                q.truncate()
+            while True:
+                chunk = q.read(READ_CHUNK)
+                if chunk == b'' or second_msg_stops_at != -1:
+                    break
+                data += chunk
+                first_msg_stops_at = data.find(MSG_END_CHAR)
+                if first_msg_stops_at != -1:
+                    second_msg_stops_at = data.find(MSG_END_CHAR, first_msg_stops_at)
 
-            return zlib.decompress(first_msg).decode('utf-8')
+            if data:
+                if first_msg_stops_at != -1:
+                    first_msg = data[0:first_msg_stops_at]
+                    if second_msg_stops_at not in (None, -1):
+                        #updates index
+                        q.seek(0)
+                        q.write(first_msg_stops_at)
+                    else:
+                        # all msgs consumed
+                        q.seek(0)
+                        q.truncate()
+                        return None
+
+                    return zlib.decompress(first_msg).decode('utf-8')
+            return None
